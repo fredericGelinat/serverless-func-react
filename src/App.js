1394dfb980c8b54845b11e4dbe8321573389773e
fredericGelinat <fredericgelinat@gmail.com>
2022-06-30 09:03:49 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Basic from './Basic'
+import Airtable from './Airtable'
+function App() {
+  return <>
+  <Basic></Basic>
+  <Airtable></Airtable>
+  </>
+}
+
+export default App
---
+++ src/Product.js
@@ -0,0 +1,54 @@
+import React,{useState, useEffect} from 'react'
+import axios from  'axios'
+import {Link, useParams} from 'react-router-dom'
+
+
+
+export const Product = () => {
+    const [loading, setloading] = useState(true);
+    const [product, setProduct]= useState(null);
+    const {productID} = useParams()
+    const fetchData = async ()=>{
+        try {
+            const {data} = await axios.get(`/api/products?id=${productID}`)
+            setProduct(data)
+        } catch (error) {
+            
+        }
+        setloading(false)
+    }
+    useEffect(()=>{
+        fetchData()
+    }, [])
+    if(loading){
+        return <section className='section section-center'>
+            <h2>loading...</h2>
+        </section>
+    }
+    const {fields} = product
+    const {name, desc, price, image} =fields
+    console.log(name)
+  return (
+    <section className='section section-center'>
+
+    <Link to="/" className='link'>
+        back home
+    </Link>
+    <div>
+        <div className='title'>
+            <h2>{name}</h2>
+            <div className='title-underline'></div>
+        </div>
+        <article className='single-product'>
+            <img className='single-product-img' src={image[0].url} alt={name}/>
+        <div>
+            <h5>{name}</h5>
+            <h5 className='price'>€{price}</h5>
+            <p>{desc}</p>
+        </div>
+        </article>
+    </div>
+    </section>
+  )
+}
+export default Product
